b64e837780931ca1d72fbbed9200986bff10f52d
Aldrik KLEBER <contact@aldrik.net>
2017-08-18 18:30:14 +0200

    sauts de lignes pour mieux mettre en évidence les différents cas de tests

--- test.ml
+++ test.ml
@@ -9,6 +9,9 @@ let test_from_params = (fun (exp,params) -> exp >:: assert_numbers exp params)
 
 let () =
 	"FizzBuzzBang game" >:::(
-		[("1",[1]) ; ("Fizz",[3;6]) ; ("Buzz",[5;10]) ; ("FizzBuzzBang",[105])]
+		[("1",[1]) ; 
+		("Fizz",[3;6]) ;
+		("Buzz",[5;10]) ;
+		("FizzBuzzBang",[105])]
 		|> List.map test_from_params)
 	|> run_test_tt_main
