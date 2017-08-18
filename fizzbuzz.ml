f0f44cbcf10b40d17d2ce414a28598d3239c805f
Aldrik KLEBER <contact@aldrik.net>
2017-08-18 18:47:13 +0200

    reduce length of code line

--- fizzbuzz.ml
+++ fizzbuzz.ml
@@ -2,10 +2,12 @@ let modulo answer modnumber n =
   if n mod modnumber = 0 then Some answer else None
 
 let engine empty f_agg rules defaultrule userinfo =
-  let applyrule r = match (r userinfo) with Some answer -> answer | None -> empty in
+  let applyrule r = match (r userinfo) with
+   Some asw -> asw | None -> empty in
   rules
   |> List.map applyrule
   |> List.fold_left f_agg empty
   |> fun s -> if s = empty then defaultrule userinfo else s
 
-let play = engine "" (^) [modulo "Fizz" 3 ; modulo "Buzz" 5 ; modulo "Bang" 7] string_of_int
+let game_rules = [modulo "Fizz" 3;modulo "Buzz" 5;modulo "Bang" 7] in
+let play = engine "" (^) game_rules string_of_int
