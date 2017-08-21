132a4e7ee9a71882f587e50426da5a1c0b4a9722
Aldrik KLEBER <contact@aldrik.net>
2017-08-21 13:12:58 +0200

    all game parameters aggregated in a record

--- fizzbuzz.ml
+++ fizzbuzz.ml
@@ -1,13 +1,27 @@
 let modulo answer modnumber n =
   if n mod modnumber = 0 then Some answer else None
 
-let engine empty f_agg rules defaultrule userinfo =
-  let applyrule r = match (r userinfo) with
-   Some asw -> asw | None -> empty in
-  rules
-  |> List.map applyrule
-  |> List.fold_left f_agg empty
-  |> fun s -> if s = empty then defaultrule userinfo else s
+type 'a engine_rules = {
+  rules : (int -> 'a option) list;
+  default_rule : (int -> 'a) ;
+  answer_agg : ('a -> 'a -> 'a) ;
+  empty : string
+}
 
-let game_rules = [modulo "Fizz" 3;modulo "Buzz" 5;modulo "Bang" 7] 
-let play = engine "" (^) game_rules string_of_int
+let fizzbuzzbang_r = {
+  rules = [modulo "Fizz" 3;modulo "Buzz" 5;modulo "Bang" 7];
+  default_rule = string_of_int ;
+  answer_agg = (^) ;
+  empty = ""
+}
+
+let engine game number =
+  let applyrule r = match (r number) with
+   Some asw -> asw | None -> game.empty in
+  game.rules
+  |> List.map applyrule
+  |> List.fold_left game.answer_agg game.empty
+  |> fun s -> if s = game.empty then game.default_rule number else s
+
+
+let play = engine fizzbuzzbang_r
