baee37508008f9d3a1c787d354c26f72a69a3549
Aldrik KLEBER <contact@aldrik.net>
2017-09-07 15:57:54 +0200

    remove unuseful variable

--- fizzbuzz.ml
+++ fizzbuzz.ml
@@ -2,9 +2,7 @@ open Game
 let multiple answer modnumber n =
   if n mod modnumber = 0 then Some answer else None
 
-let fizzbuzzbang_rules = {
+let play = {
   rules = [multiple "Fizz" 3 ; multiple "Buzz" 5; multiple "Bang" 7];
   default_rule = string_of_int ;
-  answer_agg = (^) ; answer_empty = ""}
-
-let play = engine fizzbuzzbang_rules
+  answer_agg = (^) ; answer_empty = ""} |> engine
