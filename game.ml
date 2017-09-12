a9a980ab578c60feed9af597176f6988db56323f
Aldrik KLEBER <contact@aldrik.net>
2017-09-12 17:26:44 +0200

    add a level of abstraction in order to be more independant of Functory library

--- game.ml
+++ game.ml
@@ -1,14 +1,15 @@
 open Functory.Cores
  let () =
    set_number_of_cores 4
+let list_map_reduce ~map ~reduce neutral = map_fold_a ~f:map ~fold:reduce neutral
 
 type 'a engine_rules = {
      rules : (int -> 'a option) list ; default_rule : (int -> 'a) ;
-     answer_agg : ('a -> 'a -> 'a) ; answer_empty : 'a}
+     f_aggregate : ('a -> 'a -> 'a) ; empty : 'a}
 
 let engine game number =
   let applyrule r = match (r number) with
-    Some asw -> asw | None -> game.answer_empty in
+    Some asw -> asw | None -> game.empty in
   game.rules
-  |> map_fold_a ~f:applyrule ~fold:game.answer_agg game.answer_empty
-  |> fun s -> if s = game.answer_empty then game.default_rule number else s
+  |> list_map_reduce ~map:applyrule ~reduce:game.f_aggregate game.empty
+  |> fun s -> if s = game.empty then game.default_rule number else s
